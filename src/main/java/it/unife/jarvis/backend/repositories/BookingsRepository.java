b7fd8d9dbcf87a5b50fd8d182e530cbb4362f84a
giuliamasina <giulia.masina@edu.unife.it>
2024-06-24 00:30:44 +0200

    commit

--- src/main/java/it/unife/jarvis/backend/repositories/BookingsRepository.java
+++ src/main/java/it/unife/jarvis/backend/repositories/BookingsRepository.java
@@ -2,7 +2,21 @@ package it.unife.jarvis.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import it.unife.jarvis.backend.models.Booking;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BookingsRepository extends JpaRepository<Booking, Long> {
+
+    @Query("SELECT b\n" +
+            "FROM Booking b JOIN Venue v ON b.venue = v\n" +
+            "WHERE v.name = ?1 OR v.id = ?2")
+    List<Booking> getBookingOfVenue(String name, Long id);
+
+    /* @Query("SELECT b\n" +
+            "FROM Booking b JOIN Personnel p ON p = b.personnel\n" +
+            "WHERE p.name = ?1")
+    List<Booking> getBookingsOfPersonnel(String name); */
+
 
 }
